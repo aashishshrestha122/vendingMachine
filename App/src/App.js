c6ec1a95c06cea771a9d8f22e0de68a8a250b2bc
Aashish <aashishshrestha12@gmail.com>
2021-08-14 21:25:28 +0545

    changes in client

--- App/src/App.js
+++ App/src/App.js
@@ -1,21 +1,17 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import Routes from "./routes/route";
-import store from "./store";
 
 import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Routes />
-        </Fragment>
-      </Router>
-    </Provider>
+    <Router>
+      <Fragment>
+        <Routes />
+      </Fragment>
+    </Router>
   );
 }
 
