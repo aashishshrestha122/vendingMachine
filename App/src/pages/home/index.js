916f12ab18eed9bc8d2707075c000a944d60dbf2
Aashish <aashishshrestha12@gmail.com>
2021-08-14 21:00:01 +0545

    client side

--- App/src/pages/home/index.js
+++ App/src/pages/home/index.js
@@ -23,7 +23,8 @@ const Home = () => {
 		position: "absolute",
 		top: "50%",
 		left: "50%",
-		transform: "translate(-50%, -50%)"
+		transform: "translate(-50%, -50%)",
+		borderRadius: '10px'
 	};
 
 	const items = {
